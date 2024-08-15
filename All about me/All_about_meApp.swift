ab92f89407748601c2636202b242b624df13a4da
Christopher Chang <cchang6066@stu.d214.org>
2024-08-15 07:18:59 -0700

    Initial Commit

---
+++ All about me/All_about_meApp.swift
@@ -0,0 +1,17 @@
+//
+//  All_about_meApp.swift
+//  All about me
+//
+//  Created by Christopher Chang on 8/15/24.
+//
+
+import SwiftUI
+
+@main
+struct All_about_meApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
